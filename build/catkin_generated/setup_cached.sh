e0c8b99ea25b0fa683dbde2dec782a7f6a82f406
chandlerblaid <eccmt70.cburgess@gmail.com>
2017-05-08 15:00:52 -0700

    Pushing ROS environment

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/ubuntu/catkin_ws/SID-1.0/devel:/opt/ros/indigo"
+export LD_LIBRARY_PATH="/opt/ros/indigo/lib:/usr/local/cuda-6.5/lib"
+export PWD="/home/ubuntu/catkin_ws/SID-1.0/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/ubuntu/catkin_ws/SID-1.0/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/ubuntu/catkin_ws/SID-1.0/src:/opt/ros/indigo/share:/opt/ros/indigo/stacks"
